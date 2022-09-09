baf71818737b68d65f1773ecf750ccb44ba56525
Miel Vandeputte <mielvdpu.vandeputte@ugent.be>
2022-09-09 18:43:59 +0200

    Navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,17 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
+
   PlatformFile? selectedFile;
+
+  double sensitivity = 60;
+  double pollingRate = 60;
+
+  final double _minRecommendedSensitivity = 10;
+  final double _maxRecommendedSensitivity = 200;
+
+  final double _minRecommendedPolingRate = 10;
+  final double _maxRecommendedPolingRate = 200;
 
   void _pickFile() async {
     FilePickerResult? result = await FilePicker.platform
@@ -86,68 +96,96 @@ class MyHomePageState extends State<MyHomePage> {
           selected: _currentIndex,
           onChanged: (i) => setState(() => _currentIndex = i),
           displayMode: PaneDisplayMode.top,
+          items: [
+            PaneItem(
+              icon: Icon(FluentIcons.microphone),
+              title: Text('Listener'),
+            ),
+            PaneItem(
+              icon: Icon(FluentIcons.settings),
+              title: Text('Settings'),
+            ),
+          ],
         ),
-        content: Container(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(children: <Widget>[
+        content: NavigationBody(index: _currentIndex, children: [
+          ScaffoldPage(
+              content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                Column(children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Text(
+                      'Song selection',
+                      style: DefaultTextStyle.of(context)
+                          .style
+                          .apply(fontSizeFactor: 2.0),
+                    ),
+                  ),
+                  Button(
+                      child: const Text('Select audio file'),
+                      onPressed: () {
+                        _pickFile();
+                      }),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                  if (selectedFile == null)
+                    const Text("No file selected")
+                  else
+                    Column(children: <Widget>[
+                      Text(selectedFile?.name.substring(
+                              0, selectedFile?.name.lastIndexOf('.')) ??
+                          ''),
+                      const Padding(padding: EdgeInsets.symmetric(vertical: 1)),
+                      Text(selectedFile?.extension ?? ''),
+                      const Padding(padding: EdgeInsets.symmetric(vertical: 1)),
+                      Text(selectedFile?.path?.substring(
+                              0, selectedFile?.path?.lastIndexOf('\\')) ??
+                          ''),
+                      const Padding(padding: EdgeInsets.symmetric(vertical: 1)),
+                      Text('${selectedFile?.size.toString() ?? '0'} bytes'),
+                    ]),
+                ]),
+                Column(
+                  children: [
                     Padding(
                       padding: const EdgeInsets.all(30.0),
                       child: Text(
-                        'Song selection',
+                        'Settings',
                         style: DefaultTextStyle.of(context)
                             .style
                             .apply(fontSizeFactor: 2.0),
                       ),
                     ),
-                    Button(
-                        child: Text('Select audio file'),
-                        onPressed: () {
-                          _pickFile();
-                        }),
+                    const Text('Sensitivity'),
+                    Slider(
+                        value: sensitivity < _minRecommendedSensitivity
+                            ? _minRecommendedPolingRate
+                            : sensitivity > _maxRecommendedSensitivity
+                                ? _maxRecommendedPolingRate
+                                : sensitivity,
+                        min: _minRecommendedSensitivity,
+                        max: _maxRecommendedSensitivity,
+                        onChanged: (s) => {
+                              setState(() => sensitivity = s.round().toDouble())
+                            }),
+                    Text(sensitivity.toString()),
                     const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-                    if (selectedFile == null)
-                      const Text("No file selected")
-                    else
-                      Column(children: <Widget>[
-                        Text(selectedFile?.name.substring(
-                                0, selectedFile?.name.lastIndexOf('.')) ??
-                            ''),
-                        const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 1)),
-                        Text(selectedFile?.extension ?? ''),
-                        const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 1)),
-                        Text(selectedFile?.path?.substring(
-                                0, selectedFile?.path?.lastIndexOf('\\')) ??
-                            ''),
-                        const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 1)),
-                        Text('${selectedFile?.size.toString() ?? '0'} bytes'),
-                      ]),
-                  ]),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(30.0),
-                        child: Text(
-                          'Sensitivity',
-                          style: DefaultTextStyle.of(context)
-                              .style
-                              .apply(fontSizeFactor: 2.0),
-                        ),
-                      ),
-                      Slider(value: 20, onChanged: (v) => {}),
-                      const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10)),
-                      Slider(value: 20, onChanged: (v) => {}),
-                      const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10)),
-                      Slider(value: 20, onChanged: (v) => {}),
-                    ],
-                  )
-                ])));
+                    const Text('Polling rate'),
+                    Slider(
+                        value: pollingRate < _minRecommendedPolingRate
+                            ? _minRecommendedPolingRate
+                            : pollingRate > _maxRecommendedPolingRate
+                                ? _maxRecommendedPolingRate
+                                : pollingRate,
+                        min: _minRecommendedPolingRate,
+                        max: _maxRecommendedPolingRate,
+                        onChanged: (s) => {
+                              setState(() => pollingRate = s.round().toDouble())
+                            }),
+                    Text(pollingRate.toString()),
+                  ],
+                )
+              ]))
+        ]));
   }
 }

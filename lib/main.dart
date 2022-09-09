8123ebe69a2c96182dce9bda7906e94349971cc2
Miel Vandeputte <mielvdpu.vandeputte@ugent.be>
2022-09-09 16:08:26 +0200

    Song import

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,153 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    center: true,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return FluentApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the`
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          brightness: Brightness.dark,
+          accentColor: Colors.orange,
+          iconTheme: const IconThemeData(size: 24)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  PlatformFile? selectedFile;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ['mp3']);
+
+    if (result != null) {
+      setState(() {
+        selectedFile = result.files.first;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return NavigationView(
+        appBar: NavigationAppBar(
+            title: const Text('Pursuit Projector'),
+            actions: DragToMoveArea(
+                child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Spacer(),
+                SizedBox(
+                    width: 138,
+                    height: 50,
+                    child: WindowCaption(
+                      brightness: Brightness.dark,
+                      backgroundColor: Colors.transparent,
+                    ))
+              ],
+            )),
+            automaticallyImplyLeading: true),
+        pane: NavigationPane(
+          selected: _currentIndex,
+          onChanged: (i) => setState(() => _currentIndex = i),
+          displayMode: PaneDisplayMode.top,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        content: Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Text(
+                        'Song selection',
+                        style: DefaultTextStyle.of(context)
+                            .style
+                            .apply(fontSizeFactor: 2.0),
+                      ),
+                    ),
+                    Button(
+                        child: Text('Select audio file'),
+                        onPressed: () {
+                          _pickFile();
+                        }),
+                    const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                    if (selectedFile == null)
+                      const Text("No file selected")
+                    else
+                      Column(children: <Widget>[
+                        Text(selectedFile?.name.substring(
+                                0, selectedFile?.name.lastIndexOf('.')) ??
+                            ''),
+                        const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 1)),
+                        Text(selectedFile?.extension ?? ''),
+                        const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 1)),
+                        Text(selectedFile?.path?.substring(
+                                0, selectedFile?.path?.lastIndexOf('\\')) ??
+                            ''),
+                        const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 1)),
+                        Text('${selectedFile?.size.toString() ?? '0'} bytes'),
+                      ]),
+                  ]),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Text(
+                          'Sensitivity',
+                          style: DefaultTextStyle.of(context)
+                              .style
+                              .apply(fontSizeFactor: 2.0),
+                        ),
+                      ),
+                      Slider(value: 20, onChanged: (v) => {}),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10)),
+                      Slider(value: 20, onChanged: (v) => {}),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10)),
+                      Slider(value: 20, onChanged: (v) => {}),
+                    ],
+                  )
+                ])));
   }
 }
